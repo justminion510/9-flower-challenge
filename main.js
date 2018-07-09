f9623d10ae670ede10d86b237b7f98da38ea6578
justminion510 <40474009+justminion510@users.noreply.github.com>
2018-07-09 09:37:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+drawing = SVG('drawing-area');
+
+drawing.ellipse(75,75 ).fill('brown')
+  .stroke('blue')
+  .center(200, 200)
+  .rotate(45)
+  drawing.line(0, 0, 150, 200)
+  .stroke('blue')
+drawing.ellipse(50, 75)
+.center(175, 289)
+drawing.ellipse(75, 50).fill('yellow')
+.center(230, 200)
+drawing.ellipse(50, 75).fill('yellow')
+
+drawing.ellipse(50, 75).fill('yellow') 
+.center(175, 300)
+
+
